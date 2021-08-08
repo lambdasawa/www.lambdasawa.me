613f2db7aa191826f78a492664edfa42396f8845
Tsubasa Irisawa <9388092+lambdasawa@users.noreply.github.com>
2021-08-09 02:13:57 +0900

    Update CSS (#7)

--- components/common/Timeline.tsx
+++ components/common/Timeline.tsx
@@ -8,7 +8,7 @@ export function Timeline(props: Props): JSX.Element {
   return (
     <div>
       {props.items.map(({ date, text, link }) => (
-        <div>
+        <div className="py-1">
           <span className="px-1 text-xs text-gray-400">{date}</span>
           {link ? <Link href={link} text={text} /> : <span>{text}</span>}
         </div>
