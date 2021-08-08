02b97109d53ba72c91208bb307cd771d46226ae9
Tsubasa Irisawa <9388092+lambdasawa@users.noreply.github.com>
2021-08-08 20:39:34 +0900

    Use Google Analytics (#3)
    * Use Google Analytics
    
    * Send page change event

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
+import { useRouter } from "next/dist/client/router";
+import { useEffect } from "react";
 import "../styles/globals.css";
+import MyDocument from "./_document";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (MyDocument.gaTrackingId) {
+      const handleRouteChange = (url, { shallow }) => {
+        (window as any).gtag("config", MyDocument.gaTrackingId, {
+          page_path: url,
+        });
+      };
+      router.events.on("routeChangeStart", handleRouteChange);
+      return () => {
+        router.events.off("routeChangeStart", handleRouteChange);
+      };
+    }
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
