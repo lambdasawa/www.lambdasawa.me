6b9a0dcf8264e61f3958d488244e3d3578aa9ad7
Tsubasa Irisawa <lambdasawa@gmail.com>
2022-05-02 09:31:05 +0900

    Responsive navbar

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -7,25 +7,43 @@ type Props = {
 };
 
 export default function Header({ about }: Props) {
+  const listItems = about.links.map(({ link, name }) => (
+    <li key={link}>
+      <Link href={link}>{name}</Link>
+    </li>
+  ));
+
   return (
     <div>
       <Head>
         <title>λ沢.me</title>
       </Head>
+
       <div className="navbar bg-base-100">
-        <div className="flex-1">
-          <Link href="/">
-            <a className="btn btn-ghost normal-case text-xl">λ沢.me</a>
-          </Link>
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {listItems}
+            </ul>
+          </div>
+          <a className="btn btn-ghost normal-case text-xl">λ沢.me</a>
         </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal p-0">
-            {about.links.map(({ link, name }) => (
-              <li key={link}>
-                <Link href={link}>{name}</Link>
-              </li>
-            ))}
-          </ul>
+        <div className="navbar-end hidden lg:flex">
+          <ul className="menu menu-horizontal p-0">{listItems}</ul>
         </div>
       </div>
     </div>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,9 @@ class MyDocument extends Document {
     return (
       <Html data-theme="mytheme">
         <Head>
+          <meta charSet="UTF-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link rel="icon" href="/favicon.ico" />
           {MyDocument.gaTrackingId && (
             <>
